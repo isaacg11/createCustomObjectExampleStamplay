1e2f76dbf1770b5f3c10afc860d08ba4a4a2f5df
isaacg11 <isaacgreywebdeveloper@gmail.com>
2015-10-18 14:01:37 -0700

    add and display object works

--- public/app.js
+++ public/app.js
@@ -6,5 +6,17 @@ function createObject(){
 	objectInstance.set('name', objectName);
 	objectInstance.save().then(function(){
 		var name = objectInstance.get('name');
+		var date = objectInstance.get('dt_create');
+		var id = objectInstance.get('_id');
+		document.getElementById("objectOutputName").innerHTML = name;
+		document.getElementById("objectOutputDate").innerHTML = date;
+		document.getElementById("objectOutputID").innerHTML = id;
+		document.getElementById("objectName").value = "";
 	});
+}
+
+function reset(){
+	document.getElementById("objectOutputName").innerHTML = '';
+	document.getElementById("objectOutputDate").innerHTML = '';
+	document.getElementById("objectOutputID").innerHTML = '';
 }
