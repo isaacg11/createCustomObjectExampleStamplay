27f754ce2bcad100a3b0e6a4500b4fe4adbeb945
isaacg11 <isaacgreywebdeveloper@gmail.com>
2015-10-20 11:35:21 -0700

    changed to book forcreate object section

--- public/app.js
+++ public/app.js
@@ -2,21 +2,27 @@ Stamplay.init('createcustomobjectutorial');
 
 function createObject(){
 	var objectName = document.getElementById('objectName').value;
-	var objectInstance = new Stamplay.Cobject('object').Model;
-	objectInstance.set('name', objectName);
+	var objectAuthor = document.getElementById('objectAuthor').value;
+	var objectInstance = new Stamplay.Cobject('book').Model;
+	objectInstance.set('title', objectName);
+	objectInstance.set('author', objectAuthor);
 	objectInstance.save().then(function(){
-		var name = objectInstance.get('name');
+		var title = objectInstance.get('title');
+		var author = objectInstance.instance.author;
 		var date = objectInstance.get('dt_create');
 		var id = objectInstance.get('_id');
-		document.getElementById("objectOutputName").innerHTML = name;
+		document.getElementById("objectOutputName").innerHTML = title;
+		document.getElementById("objectOutputAuthor").innerHTML = author;
 		document.getElementById("objectOutputDate").innerHTML = date;
 		document.getElementById("objectOutputID").innerHTML = id;
 		document.getElementById("objectName").value = "";
+		document.getElementById("objectAuthor").value = "";
 	});
 }
 
 function reset(){
 	document.getElementById("objectOutputName").innerHTML = '';
+	document.getElementById("objectOutputAuthor").innerHTML = '';
 	document.getElementById("objectOutputDate").innerHTML = '';
 	document.getElementById("objectOutputID").innerHTML = '';
 }
